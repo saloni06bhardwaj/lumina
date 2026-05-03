17f3087d930c0841bbd0e4150f83eb941c8fb48f
Saloni Bhardwaj <salonibhardwaj0603@gmail.com>
2026-05-03 18:37:56 +0530

    Fix deployment issue by adding a root endpoint to the server.

--- project-management-backend/server.js
+++ project-management-backend/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('API is running 🚀');
+});
+
 const authroutes = require('./routes/auth');
 const projectRoutes = require('./routes/project');
 
